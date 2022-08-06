a20b9aa08d8a533c747cfe71cd10b5456c94ce58
Brian Ketelsen <bketelsen@gmail.com>
2022-08-06 09:17:16 -0400

    sd

--- modules/home-manager/dotfiles/default.nix
+++ modules/home-manager/dotfiles/default.nix
@@ -5,6 +5,18 @@
       target = ".zfunc";
       recursive = true;
     };
+
+    sd = {
+      recursive = true;
+      target = ".sd";
+
+      source = pkgs.fetchFromGitHub {
+        owner = "ianthehenry";
+        repo = "sd";
+        rev = "ecd1ab8";
+        sha256 = "sha256-asxdd+ZSqt3GEP9uuITVbtwr800kBHLe1Jg=";
+      };
+    };
     npmrc = {
       text = ''
         prefix = ${config.home.sessionVariables.NODE_PATH};
