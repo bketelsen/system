866e4927c370d53e6bcc0d3e2acccbdf528cd965
Brian Ketelsen <bketelsen@gmail.com>
2022-08-08 02:42:03 -0400

    fmt

--- modules/hardware/beast.nix
+++ modules/hardware/beast.nix
@@ -5,9 +5,10 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       (modulesPath + "/installer/scan/not-detected.nix")
-       ./zfs.nix
+      ./zfs.nix
     ];
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
@@ -17,37 +18,49 @@
   networking.hostName = "beast"; # Define your hostname.
 
   fileSystems."/" =
-    { device = "rpool/nixos/root";
-      fsType = "zfs"; options = [ "zfsutil" "X-mount.mkdir" ];
+    {
+      device = "rpool/nixos/root";
+      fsType = "zfs";
+      options = [ "zfsutil" "X-mount.mkdir" ];
     };
 
   fileSystems."/home" =
-    { device = "rpool/nixos/home";
-      fsType = "zfs"; options = [ "zfsutil" "X-mount.mkdir" ];
+    {
+      device = "rpool/nixos/home";
+      fsType = "zfs";
+      options = [ "zfsutil" "X-mount.mkdir" ];
     };
 
   fileSystems."/var/lib" =
-    { device = "rpool/nixos/var/lib";
-      fsType = "zfs"; options = [ "zfsutil" "X-mount.mkdir" ];
+    {
+      device = "rpool/nixos/var/lib";
+      fsType = "zfs";
+      options = [ "zfsutil" "X-mount.mkdir" ];
     };
 
   fileSystems."/var/log" =
-    { device = "rpool/nixos/var/log";
-      fsType = "zfs"; options = [ "zfsutil" "X-mount.mkdir" ];
+    {
+      device = "rpool/nixos/var/log";
+      fsType = "zfs";
+      options = [ "zfsutil" "X-mount.mkdir" ];
     };
 
   fileSystems."/boot" =
-    { device = "bpool/nixos/root";
-      fsType = "zfs"; options = [ "zfsutil" "X-mount.mkdir" ];
+    {
+      device = "bpool/nixos/root";
+      fsType = "zfs";
+      options = [ "zfsutil" "X-mount.mkdir" ];
     };
 
   fileSystems."/boot/efis/nvme-Samsung_SSD_980_PRO_1TB_S5P2NG0R914459M-part1" =
-    { device = "/dev/disk/by-uuid/5D2B-7A3F";
+    {
+      device = "/dev/disk/by-uuid/5D2B-7A3F";
       fsType = "vfat";
     };
 
   fileSystems."/boot/efi" =
-    { device = "/boot/efis/nvme-Samsung_SSD_980_PRO_1TB_S5P2NG0R914459M-part1";
+    {
+      device = "/boot/efis/nvme-Samsung_SSD_980_PRO_1TB_S5P2NG0R914459M-part1";
       fsType = "none";
       options = [ "bind" ];
     };
@@ -57,11 +70,10 @@
   networking.interfaces.enp5s0.useDHCP = true;
   networking.interfaces.br0.useDHCP = true;
   networking.bridges = {
-  "br0" = {
-    interfaces = [ "enp6s0" ];
+    "br0" = {
+      interfaces = [ "enp6s0" ];
+    };
   };
-  };
-
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
