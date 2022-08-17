4c2543f1a1c84979186f5933950f207a1d585c41
Brian Ketelsen <bketelsen@gmail.com>
2022-08-17 19:47:34 -0400

    update astrovim

--- modules/home-manager/dotfiles/default.nix
+++ modules/home-manager/dotfiles/default.nix
@@ -37,7 +37,7 @@
         owner = "AstroNvim";
         repo = "AstroNvim";
         rev = "25fa8d7";
-        sha256 = "sha256-+jCoSx9EBLvhY9Zz41Y6cCmrSSI66Fud8rzslELOtfs=";
+        sha256 = "sha256-up5vyQn07TiWvywwYRIlOhn3e3w49FalUMLqsjW/7IA=";
       };
     };
   };
