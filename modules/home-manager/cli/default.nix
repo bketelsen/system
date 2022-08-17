3737013560021b6834b9b6c2ad3aa23cb0442742
Brian Ketelsen <bketelsen@gmail.com>
2022-08-17 04:21:01 -0400

    test starship

--- modules/home-manager/cli/default.nix
+++ modules/home-manager/cli/default.nix
@@ -131,6 +131,17 @@ in
     starship = {
       enable = true;
       package = pkgs.starship;
+      settings = {
+        add_newline = true;
+
+       character = {
+         success_symbol = "[➜](bold green)";
+         error_symbol = "[➜](bold red)";
+       };
+
+      # package.disabled = true;
+    };
+  };
     };
   };
 }
