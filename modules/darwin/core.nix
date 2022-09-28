90cbb42abdfb4805901be0f43a61d037ee18ef97
brian ketelsen <bketelsen@gmail.com>
2022-09-28 08:22:44 -0400

    enable syncthing

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -28,6 +28,7 @@ in
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
+  services.syncthing.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -4,5 +4,6 @@
     ./core.nix
   #  ./brew.nix
     ./preferences.nix
+    ./syncthing.nix
   ];
 }
