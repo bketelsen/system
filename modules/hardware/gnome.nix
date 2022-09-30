07d51d10e4d5fdb076d35f2c31bdf8fef2bc5973
Brian Ketelsen <bketelsen@gmail.com>
2022-09-29 21:33:55 -0400

    firefox

--- modules/hardware/gnome.nix
+++ modules/hardware/gnome.nix
@@ -52,7 +52,16 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
+xdg = {
+  portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+    gtkUsePortal = true;
+  };
+};
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
