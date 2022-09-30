ff59150267e220cefe94e63a118b30803245974d
Brian Ketelsen <bketelsen@gmail.com>
2022-09-30 19:54:53 -0400

    docker ghost

--- modules/hardware/selfhost.nix
+++ modules/hardware/selfhost.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 {
     virtualisation.oci-containers.containers = {
+        db = {
+            image = "mysql:8";
+            ports = ["3306:3306"];
+
+        };
         ghost = {
             image = "ghost:5.17";
             dependsOn = [ "db" ];
@@ -10,9 +15,6 @@
             };
             ports = [ "2368:2368" ];
         };
-        db = {
-            image = "mysql:8";
 
-        };
     };
 }
