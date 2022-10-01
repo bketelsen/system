24397c2349a88f657dc73de106fb911e89d562cc
Brian Ketelsen <bketelsen@gmail.com>
2022-09-30 20:03:47 -0400

    docker ghost

--- modules/hardware/selfhost.nix
+++ modules/hardware/selfhost.nix
@@ -16,8 +16,11 @@
             image = "ghost:5.17";
             dependsOn = [ "db" ];
             environment = {
-                DATABASE_HOST = "db.example.com";
-                DATABASE_PORT = "3306";
+                database__client: "mysql";
+                database__connection__host: "db";
+                database__connection__user: "ghost";
+                database__connection__password: "gh0st";
+                database__connection__database: "ghost";
             };
             ports = [ "2368:2368" ];
             autoStart = true;
